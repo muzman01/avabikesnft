099e6b8c0d0ad55e6cab92555f8f244f39288c7e
muzman01 <supurge_man@hotmail.com>
2022-06-24 11:19:45 +0300

    mobile baya baya

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import dc from "../../public/images/discord.png";
 import tw from "../../public/images/tw.png";
@@ -6,17 +6,45 @@ import tl from "../../public/images/tl.png";
 import yt from "../../public/images/yt.png";
 import log from "../../public/images/ftlogo.png";
 import AOS from "aos";
+import { Modal, Button } from "react-bootstrap";
 import { useEffect } from "react";
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="resimengel">yazııı</div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 function Footer() {
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
-      }, []);
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+  const [modalShow, setModalShow] = useState(false);
   return (
     <section>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
       <div className="d-flex justify-content-around mt-5">
         <div className="faqlink" data-aos="fade-right">
-          <h1 className="faq">FAQ</h1>
+          <h1 className="faq" onClick={() => setModalShow(true)} style={{cursor:"pointer"}} >
+            FAQ
+          </h1>
           <Image src={tw} height={40} width={40} />
           <Image src={dc} height={40} width={40} />
           <Image src={tl} height={40} width={40} />
@@ -24,7 +52,9 @@ function Footer() {
         </div>
 
         <div className="Go_To_APP" data-aos="fade-left">
-          <a href="#home"><Image src={log} /></a>
+          <a href="#home">
+            <Image src={log} />
+          </a>
         </div>
       </div>
     </section>
